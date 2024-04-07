e3af0369edb251456bc7b5f92a5d64ea1ae50f0a
Mitchell Christ <xzmaru@gmail.com>
2024-04-07 13:39:32 -0700

    fix sitemap

--- next/src/app/sitemap.ts
+++ next/src/app/sitemap.ts
@@ -5,7 +5,7 @@ import type { MetadataRoute } from 'next'
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 	const allPages = await fetchSanity<Record<string, MetadataRoute.Sitemap>>(
 		groq`{
-			'pages': *[_type == 'page' && !(metadata.slug.current in ['404']) && noIndex != false]|order(metadata.slug.current){
+			'pages': *[_type == 'page' && !(metadata.slug.current in ['404']) && noIndex != true]|order(metadata.slug.current){
 				'url': $baseUrl + select(metadata.slug.current == 'index' => '', metadata.slug.current),
 				'lastModified': _updatedAt,
 				'priority': select(
@@ -20,6 +20,8 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 			},
 		},
 	)
+
+	console.log({ allPages })
 
 	return Object.values(allPages).flat()
 }
